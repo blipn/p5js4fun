df162b5a11cee7921b71273f1f07bfc9c8504781
blipn <oblipn@gmail.com>
2021-05-20 15:07:53 +0200

    add twitchParticles chat only without login

---
+++ box/7-twitchParticlesChatOnly/mover.js
@@ -0,0 +1,28 @@
+class Mover {
+  constructor(x, y, m, c) {
+    this.pos = createVector(x, y);
+    this.vel = p5.Vector.random2D();
+    this.vel.mult(5);
+    this.acc = createVector(0, 0);
+    this.mass = m;
+    this.color = c;
+    this.r = sqrt(this.mass) * 2;
+  }
+
+  applyForce(force) {
+    let f = p5.Vector.div(force, this.mass);
+    this.acc.add(f);
+  }
+
+  update() {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    this.acc.set(0, 0);
+  }
+
+  show() {
+    noStroke();
+    this.color ? fill(this.color) : fill(this.pos.x/2, this.mass, this.pos.y/2);
+    ellipse(this.pos.x, this.pos.y, this.r * 2);
+  }
+}
---
+++ box/7-twitchParticlesChatOnly/sketch.js
@@ -0,0 +1,68 @@
+let movers = []
+let attractors = []
+let reflect = 0
+const maxMovers = 1000
+
+function setup() {
+  createCanvas(windowWidth, windowHeight)
+  attractors.push(new Attractor(width / 2, height / 2, 10))
+  for (let i = 0; i < 1; i++) {
+    popOne()
+  }
+  background(0)
+}
+
+function draw() {
+  background(0,0,0,reflect)
+  for (let mover of movers) {
+    mover.update()
+    mover.show()
+    attractors.forEach(a => a.attract(mover))
+  }
+  if (mouseIsPressed) {
+    attractors[1] = new Attractor(mouseX, mouseY, 2)
+  }
+  attractors.forEach(a => a.show())
+}
+
+function popOne(mass, color) {
+  if(movers.length >= maxMovers) return
+  let x = random(width/2.2, width/1.8)
+  let y = random(height/2.2, height/1.8)
+  let m = mass || random(0.1, 1)
+  movers.push(new Mover(x, y, m, color))
+  reflect = (movers.length + 10) / 5
+}
+
+function removeOne() {
+  movers.splice(0,1)
+  reflect = (movers.length + 2) / 5
+}
+
+const loginForm = document.getElementById('login')
+const output = document.getElementById('output')
+loginForm.onsubmit = (event) => {
+  event.preventDefault()
+  const inputs = loginForm.elements
+  const settings = {}
+  Array.from(inputs).forEach((item)=>{
+      if(item.type === 'submit') {
+        item.disabled = true
+      } else if (item.nodeName === 'INPUT') {
+        settings[item.name] = item.value
+      }
+  })
+
+  const client = new tmi.Client({
+    channels: [ settings.user_login ]
+  })
+  client.connect()
+  client.on('message', (channel, tags, message, self) => {
+    console.log(tags)
+    console.log(`${tags['display-name']}: ${message}`)
+    popOne(random(0.2, 2), tags['color'])
+    setTimeout(()=>{
+      removeOne()
+    }, 60000)    
+  })
+}
